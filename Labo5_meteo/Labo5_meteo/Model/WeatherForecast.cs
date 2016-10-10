f67f8678477ef5b974cfb35ad38649f2e7a84113
Valentin <Valentin.veny.01@student.henallux.be>
2016-10-10 12:30:59 +0200

    10/10/2016 -> fin cours

---
+++ Labo5_meteo/Labo5_meteo/Model/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo5_meteo.Model
+{
+    public class WeatherForecast
+    {
+        public DateTime date { get; set; }
+        public double MinTemp { get; set; }
+        public double MaxTemp { get; set; }
+        public double WindSpeed { get; set; }
+        public string WeatherDescription { get; set; }
+
+
+    }
+}
---
+++ Labo5_meteo/Labo5_meteo/Model/WeatherService.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo5_meteo.Model
+{
+    class WeatherService
+    {
+        public async Task<IEnumerable<WeatherForecast>> GetForecast()
+        {
+            var wc = new HttpClient();
+            var weather = await wc.GetStringAsync(new Uri("http://api.openweathermap.org/data/2.5/forecast?q=Namur, be&lang=fr&units=metric&mode=json&appid=294161c723600d2f95fea5c88d5ffb0b");
+            var rawWeather = JObject.Parse(weather);
+            /*
+            var forecast = rawWeather ["list"].children().Select(d => new WeatherForecast)
+                {
+                    
+                }
+            */
+        }
+    }
+}
